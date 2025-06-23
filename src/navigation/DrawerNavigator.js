1261648c2ecc723e7941d980e945cf99e69d715c
Divyanshu Kushwaha <divyanshu4381@gmail.com>
2025-06-23 12:03:59 +0530

    feat: Add authentication protection to DrawerNavigator

--- src/navigation/DrawerNavigator.js
+++ src/navigation/DrawerNavigator.js
@@ -1,24 +1,37 @@
 // src/navigation/DrawerNavigator.js
 import React from 'react';
+import 'react-native-gesture-handler';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import BottomTabNavigator from './BottomTabNavigator';
 import ProfileScreen from '../screens/ProfileScreen';
-
+import { useUser } from '../context/AuthContext';
+import { useNavigation } from '@react-navigation/native';
 const Drawer = createDrawerNavigator();
 
 const DrawerNavigator = () => {
+  const navigation = useNavigation();
+  const { user } = useUser();
+  const AuthProtect=({children})=>{
+    if(!user){
+      navigation.navigate('Login');
+      return null;
+    }
+    return children;
+  }
   return (
-    <Drawer.Navigator
-      screenOptions={{
-        headerShown: false,
-        drawerType: 'front',
-        drawerPosition: 'left',
-      }}
-    >
-      <Drawer.Screen name="Main" component={BottomTabNavigator} />
-      <Drawer.Screen name="Profile" component={ProfileScreen} />
-      
-    </Drawer.Navigator>
+    <AuthProtect>
+      <Drawer.Navigator
+        screenOptions={{
+          headerShown: false,
+          drawerType: 'front',
+          drawerPosition: 'left',
+        }}
+      >
+        <Drawer.Screen name="Main" component={BottomTabNavigator} />
+        <Drawer.Screen name="Profile" component={ProfileScreen} />
+
+      </Drawer.Navigator>
+    </AuthProtect>
   );
 };
 
